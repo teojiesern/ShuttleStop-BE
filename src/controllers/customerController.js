dc98018fbf2aacf0c233f8ef52d54e7e82f5612a
teojiesern <teojiesern42@gmail.com>
2024-05-06 23:01:24 +0800

    task(customer): registration logic

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,10 +1,32 @@
 const customerService = require('../services/customerService');
+const Customer = require('../models/CustomerSchema');
+const errorHandler = require('../handlers/dbErrorHandlers');
 
-exports.getCustomer = async (req, res) => {
+const getCustomer = async (req, res) => {
     try {
         const examples = await customerService.getCustomer(req.cookies['shuttle-token']);
         res.json(examples);
     } catch (error) {
-        res.status(500).json({ error: 'Internal Server Error' });
+        res.status(500).json({ type: 'internal-server-error' });
     }
+};
+
+const createCustomer = async (req, res) => {
+    // name, email, password
+    const customer = new Customer(req.body);
+    try {
+        await customer.save();
+        return res.status(200).json({
+            message: 'Successfully signed up!',
+        });
+    } catch (err) {
+        return res.status(500).json({
+            type: 'unable-to-create-user',
+        });
+    }
+};
+
+module.exports = {
+    getCustomer,
+    createCustomer,
 };
--- src/routes/customerRoutes.js
+++ src/routes/customerRoutes.js
@@ -4,5 +4,6 @@ const customerController = require('../controllers/customerController');
 const ciamController = require('../controllers/ciamController');
 
 router.route('/').get(ciamController.hasCookie, customerController.getCustomer);
+router.route('/signup').post(customerController.createCustomer);
 
 module.exports = router;
