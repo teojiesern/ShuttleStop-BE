3fb1a4ef24868be4d8f907ed659215e5ffa8c722
teojiesern <teojiesern42@gmail.com>
2024-05-10 23:35:19 +0800

    feat(model): add shop schema

---
+++ src/models/ShopSchema.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const ShopSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true,
+        required: 'Name is required',
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    logo: {
+        data: Buffer,
+        contentType: String,
+    },
+    supportedStandardDelivery: {
+        type: [String],
+        validate: {
+            validator: (v) => v.length > 0,
+            message: 'At least one supported standard delivery is required',
+        },
+    },
+    supportedShippingMethod: {
+        type: [String],
+        validate: {
+            validator: (v) => v.length > 0,
+            message: 'At least one supported shipping method is required',
+        },
+    },
+    pickupAddress: {
+        type: String,
+        required: 'Pickup address is required',
+    },
+    email: {
+        type: String,
+        trim: true,
+        unique: true,
+        match: [/.+\@.+\..+/, 'Please fill a valid email address'],
+        required: 'Email is required',
+    },
+    products: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Product',
+        },
+    ],
+    owner: { type: String, ref: 'Seller' },
+});
+
+const Shop = mongoose.model('Shop', ShopSchema);
+
+module.exports = Shop;
