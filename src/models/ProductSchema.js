ee2cb56d16bbd661fc7dbc7655e1ee038f8bb020
teojiesern <teojiesern42@gmail.com>
2024-05-19 07:38:29 +0800

    new product schema

--- src/models/ProductSchema.js
+++ src/models/ProductSchema.js
@@ -1,27 +1,61 @@
 const mongoose = require('mongoose');
 
+const VariantSchema = new mongoose.Schema({
+    color: {
+        type: String,
+        required: true,
+    },
+    totalStock: {
+        type: Number,
+        required: true,
+    },
+    totalSales: {
+        type: Number,
+        required: true,
+    },
+    price: {
+        type: Number,
+        required: true,
+    },
+});
+
 const ProductSchema = mongoose.Schema(
     {
         name: {
             type: String,
             required: [true, 'Please enter product name'],
         },
-
-        quantity: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-
-        price: {
-            type: Number,
-            required: true,
-            default: 0,
-        },
-
-        image: {
+        category: {
             type: String,
-            required: false,
+            enum: ['RACQUETS', 'FOOTWEARS', 'APPARELS', 'BAGS', 'SHUTTLECOCKS', 'ACCESSORIES'],
+            required: 'Category is required',
+        },
+        brands: {
+            type: String,
+            enum: ['YONEX', 'LI_NING', 'VICTOR', 'ASICS', 'APACS', 'FZ_FORZA', 'KAWASAKI'],
+            required: 'Brand is required',
+        },
+        thumbnailImage: {
+            type: String,
+            required: 'Thumbnail image is required',
+        },
+        productImages: {
+            type: [String],
+            validate: {
+                validator: (v) => v.length > 0,
+                message: 'At least one product image is required',
+            },
+        },
+        productDescription: {
+            type: String,
+            required: 'Product description is required',
+        },
+        variants: {
+            type: [VariantSchema],
+            validate: {
+                validator: (v) => v.length > 0,
+                message: 'At least one product image is required',
+            },
         },
     },
     {
