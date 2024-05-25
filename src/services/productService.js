7e6660e6c859680f4a42f08ea69804fc31ca5f7c
chiew256 <yityenw@gmail.com>
2024-05-25 16:26:42 +0800

    get product from db

---
+++ src/services/productService.js
@@ -0,0 +1,20 @@
+const Product = require('../models/ProductSchema');
+
+const getAllProducts = async () => {
+    const products = await Product.find({});
+    console.log(products);
+    return products;
+};
+
+const getProductById = async (productId) => {
+    const product = await Product.findOne({ productId: productId });
+    if (!product) {
+        throw new Error('Product not found');
+    }
+    return product;
+};
+
+module.exports = {
+    getAllProducts,
+    getProductById,
+};
